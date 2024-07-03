44801eb72e4cef7b78dae8aafeba14b44fed51e1
Red Williams <red@gencitylabs.io>
2024-07-03 11:25:49 -0700

    add player and environment resources

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private Button _StartBtn;
     [SerializeField] private Button _OptionsBtn;
+    private string GameScene = "GameScene";
     // Start is called before the first frame update
     void Start()
     {
-        
+        _StartBtn = GetComponent<Button>();
+
+        _OptionsBtn = GetComponent<Button>();
+
+        if (_StartBtn != null )
+        {
+            _StartBtn.onClick.AddListener(LoadGameScene);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void LoadGameScene()
     {
-        
+        SceneManager.LoadScene(GameScene);
     }
 }
