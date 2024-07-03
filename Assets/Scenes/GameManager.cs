8c29386c50c2e567e2ff41595a301068392fdcb5
Red Williams <red@gencitylabs.io>
2024-07-03 10:52:45 -0700

    add buttons to menu UI, add script for gamemanager

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private Button _StartBtn;
+    [SerializeField] private Button _OptionsBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
